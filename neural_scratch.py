093f87fd2b1d1dc02ce584ead470277f3304b82c
Sushi <susheendharvj@gmail.com>
2020-02-02 00:07:06 +0000

    backpropagation

--- neural_scratch.py
+++ neural_scratch.py
@@ -1,4 +1,7 @@
 import numpy as np 
+import mnist_loader
+import random 
+
 
 class Network(object):
 	def __init__(self,sizes):
@@ -40,15 +43,62 @@ class Network(object):
 			nabla_b = [nb+dnb for nb,dnb in zip(nabla_b,delta_nabla_b)]
 			nabla_w = [nw + dnw for nw,dnw in zip(nabla_w,delta_nabla_w)]
 		self.weights = [w- (eta/len(mini_batch))*nbw for w,nbw in zip(self.weights,nabla_w)]
-		self.biases = [b - (eta/len(mini_batch))*nb for b,nbb in zip(self.biases,nabla_b)] 
+		self.biases = [b - (eta/len(mini_batch))*nb for b,nb in zip(self.biases,nabla_b)] 
 		
+
+
+	def backprop(self,x,y):
+		nabla_b = [np.zeros(b.shape) for b in self.biases]
+		nabla_w = [np.zeros(w.shape) for w in self.weights]
+
+		activation = x
+		activations = [x]
+		zs = []
+		for b,w in zip(self.biases,self.weights):
+			z = np.dot(w,activation) + b
+			zs.append(z)
+			activation = sigmoid(z)
+			activations.append(activation)
+
+		delta = self.cost_derivation(activations[-1],y) * sigmoid_prime(zs[-1])
+		nabla_b[-1] = delta
+		nabla_w[-1] = np.dot(delta,activations[-2].transpose())
+
+		for l in range(2,self.num_layers):
+			z = zs[-l]
+			sp = sigmoid_prime(z)
+			delta = np.dot(self.weights[-l+1].transpose(),delta) * sp
+			nabla_b[-l]  = delta 
+			nabla_w[-l] = np.dot(delta,activations[-l-1].transpose())
+
+		return (nabla_b,nabla_w)
+
+
+
+
+	def cost_derivation(self,output_activations,y):
+		return (output_activations - y)
+
+	def evaluate(self,testing_data):
+		predictions  = [(np.argmax(self.feedforward(x)),y) for x,y in testing_data]
+		return sum([int(x==y) for (x,y) in predictions]) 
 
 
 	
 
 
-
+def sigmoid_prime(z):
+	return sigmoid(z)*(1-sigmoid(z))
 
 
 def sigmoid(z):
 	return 1.0/(1.0+ np.exp(-z))
+
+
+
+
+training_data,validation_data,testing_data = mnist_loader.load_data_wrapper()
+net = Network([784,5,10])
+
+net.SGD(training_data,90,30,3,test_data=testing_data)
+
